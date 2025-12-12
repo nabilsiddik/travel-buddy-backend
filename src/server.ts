57f2985b09b510173ffa5e1a53233daf0f2b1a86
nabilsiddik <nabilsiddik90@gmail.com>
2025-12-12 21:53:57 +0600

    changed port

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,8 @@ import { envVars } from "./app/config/env.config.js"
 
 const startServer = async () => {
     try {
-        app.listen(envVars.PORT, () => {
+        const port = process.env.PORT || 5000;
+        app.listen(port, () => {
             console.log(`Server is listening to port ${envVars.PORT}`)
         })
     } catch (error) {
