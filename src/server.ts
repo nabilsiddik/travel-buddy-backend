97b4548e80863814190f1cc0bc9568da2a77940d
nabilsiddik <nabilsiddik90@gmail.com>
2025-12-12 18:08:01 +0600

    render deploy

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
-import { app } from "./app"
-import { envVars } from "./app/config/env.config"
+import { app } from "./app.js"
+import { envVars } from "./app/config/env.config.js"
 
 const startServer = async () => {
     try {
